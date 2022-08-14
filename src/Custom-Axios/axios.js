61d4d58a356adafe4933899e713085cbf1656b87
delenikov <delenikov.stojan@gmail.com>
2022-08-14 20:11:33 +0200

    Adapt Localhost

--- src/Custom-Axios/axios.js
+++ src/Custom-Axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create ({
-    baseURL: 'https://kriptopedija-backend.herokuapp.com/',
+    baseURL: 'http://localhost:8080',
     headers: {
         'Access-Control-Allow-Origin' : '*',
         'Authorization': localStorage.getItem("JWT")
