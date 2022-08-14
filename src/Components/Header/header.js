63cc42670dcfd67807492f185b571161217543a7
delenikov <delenikov.stojan@gmail.com>
2022-08-14 20:04:01 +0200

    Fixing minor bug

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -24,6 +24,13 @@ const Header = (props) => {
 
     return (
         <header>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+                  integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+                  crossOrigin="anonymous"/>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+                    crossOrigin="anonymous"/>
+
             <nav className="navbar navbar-expand-lg bg-black">
                 <div className="container-fluid">
                     <a className="navbar-brand text-white" href="/">Криптопедија</a>
