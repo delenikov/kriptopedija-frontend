e72661deea085297a625875caed18fc98de35499
delenikov <delenikov.stojan@gmail.com>
2022-08-14 19:57:34 +0200

    Change a tag

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -35,7 +35,7 @@ const Header = (props) => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link text-primary" to={"/courses"}>Курсеви</Link>
+                                <a className="nav-link text-primary" href="/courses">Курсеви</a>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link text-primary" to={"/crypto"}>Криптовалути</Link>
